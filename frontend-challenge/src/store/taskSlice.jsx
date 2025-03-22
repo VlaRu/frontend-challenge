af31fc49540c9b8e7444900d19f6eb8590853652
VlaRu <dn210690nvs@gmail.com>
2025-03-22 15:10:05 +0300

    feat: initialize Redux store and add task slice rtk

---
+++ frontend-challenge/src/store/taskSlice.jsx
@@ -0,0 +1,39 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const taskSlice = createSlice({
+  name: 'tasks',
+  initialState: {
+    value: [],
+    count: 0,
+  },
+  reducers: {
+    addNewTask: (state, action) => {
+      if (!Array.isArray(state.value)) state.value = [];
+      state.value.push({
+        id: crypto.randomUUID(),
+        text: action.payload,
+        completed: false,
+      });
+      state.count = state.value.length;
+    },
+    deleteTask: (state, action) => {
+      state.value = state.value.filter((task) => task.id !== action.payload);
+      state.count = state.value.length;
+    },
+    toggleTask: (state, action) => {
+      const task = state.value.find((task) => task.id === action.payload);
+      if (task) {
+        task.completed = !task.completed;
+      }
+      state.count = state.value.length;
+    },
+    clearCompletedTasks: (state) => {
+      state.value = state.value.filter((task) => !task.completed);
+      state.count = state.value.length;
+    },
+  },
+});
+
+export const { addNewTask, deleteTask, toggleTask, clearCompletedTasks } =
+  taskSlice.actions;
+export default taskSlice.reducer;
