7e7bbe94a5264867bdf901eba54090cef9b6614e
VlaRu <dn210690nvs@gmail.com>
2025-03-23 21:30:52 +0300

    feat: add style for companents

--- frontend-challenge/src/components/Header.jsx
+++ frontend-challenge/src/components/Header.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import moonIcon from '../assets/icons/icon-moon.svg';
 import sunIcon from '../assets/icons/icon-sun.svg';
+import '../styles/Header.css'
 
 export function Header() {
   const [iconTheme, setIconTheme] = useState(moonIcon)
@@ -9,10 +10,10 @@ export function Header() {
     setIconTheme((prevTheme) => (prevTheme === moonIcon ? sunIcon : moonIcon))
   }
   return (
-    <header>
+    <header className='header-container'>
       <h1>TODO</h1>
       <div onClick={handleThemeIcon}>
-        <img src={iconTheme} alt="theme-img"/>
+        <img className='theme-icon' src={iconTheme} alt="theme-img"/>
       </div>
     </header>
   )
